4c98bc3bc1d3a9cabb98884d3e892784f3bcb214
atatum718 <54254894+atatum718@users.noreply.github.com>
2020-09-05 21:02:00 -0400

    added buttons to the dog name saying pet me

--- src/App.js
+++ src/App.js
@@ -16,22 +16,42 @@ class App extends Component {
         name: "Wookie",
       },
     ],
+    newDogName: "",
   };
 
+  handleChangeDogName = (event) => {
+    this.setState({ newDogName: event.target.value });
+  };
+
+  adoptDog = (event) => {
+    this.setState((state) => {
+      return {
+        dogs: [...state.dogs, { name: state.newDogName }],
+        newDogName: "",
+      };
+    });
+  };
   render() {
     return (
       <div className="app">
         <Header numDogs={this.state.dogs.length} />
         <h2>My Dogs</h2>
         <ul className="dogList">
-          {this.state.dogs.map((dog) => (
-            <li>
+          {this.state.dogs.map((dog, index) => (
+            <li key={index}>
               <Dog dog={dog} />
             </li>
           ))}
         </ul>
         <br />
-        <div>Add Input and button here!</div>
+        <div>
+          <input
+            type="text"
+            onChange={this.handleChangeDogName}
+            value={this.state.newDogName}
+          ></input>
+          <button onClick={this.adoptDog}>Adopt a dog</button>
+        </div>
       </div>
     );
   }
